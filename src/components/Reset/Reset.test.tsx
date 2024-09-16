aa326907e23a1984b27c41936eb2799ca61cf937
RamonMaximiliano <ramon_max1986@hotmail.com>
2024-09-16 15:51:09 -0300

    feat: reset test started

--- src/components/Reset/Reset.test.tsx
+++ src/components/Reset/Reset.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent} from '@testing-library/react';
 import { Reset } from './Reset';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { DBContext } from '../../DataBase/database';
 import "@testing-library/jest-dom"
 
 describe("Login testing", ()=>{
@@ -9,6 +10,28 @@ describe("Login testing", ()=>{
         render(<BrowserRouter><Reset/></BrowserRouter>);
         const ResetID = screen.getByText(/Change your password to make the world a better place/i);
         expect(ResetID).toBeInTheDocument();
+    });
+
+    test("If click 'Login now' takes back to Login",()=>{
+        render(
+            <DBContext.Provider  value={{ 
+                users: [],
+                setUsers: jest.fn(),
+                logged: { name: "Max", email: "max@example.com", password: "password" }, 
+                setLogged: jest.fn()
+                }}>
+          {/* By setting initialEntries={['/logged']}, the router behaves as if the App is on the /logged page when the test starts. This simulates the user being logged in and viewing the "Logged" page. */}
+                <MemoryRouter initialEntries={['/Reset']}>
+                    <Reset />
+                </MemoryRouter>
+            </DBContext.Provider>
+        );
+        const backLogin = screen.getByText(/Login now/i);
+        expect(backLogin).toBeInTheDocument();
+        fireEvent.click(backLogin);
+        // Check that navigation happened
+        expect(window.location.pathname).toBe("/");
     })
+
 })
 
